094067ef6adab3b276716b9a0f86921da36c7fda
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-07-27 18:18:07 +0700

    feat: analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./component/Navbar";
 import Hero from "./component/Hero";
+import Analytics from "./component/Analytics";
 
 function App() {
   return (
     <div className="App bg-gray-800 min-h-screen w-screen">
       <Navbar />
       <Hero />
+      <Analytics />
     </div>
   );
 }
