58ca0304009c5e6e51478b1798dd0bed8d4d4109
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-07-27 15:23:40 +0700

    feat: Hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./component/Navbar";
+import Hero from "./component/Hero";
 
 function App() {
   return (
     <div className="App bg-gray-800 min-h-screen w-screen">
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/component/Hero.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { ReactTyped } from "react-typed";
+
+const Hero = () => {
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#00df9a] font-bold p-2">GROWING WITH DATA ANALYTICS</p>
+        <h1 className="md:text-7xl sm:text-6xl text-5xl font-bold md:py-6">Grow with data.</h1>
+        <div className="flex justify-center md:mt-0 mt-4">
+          <p className="md:text-4xl sm:text-3xl text-xl font-bold">Fast, flexible financing for</p>
+          <ReactTyped className="md:text-4xl sm:text-3xl text-xl font-bold md:pl-4 pl-2" strings={["BTB", "BTC", "SASS"]} typeSpeed={120} backSpeed={140} loop />
+        </div>
+        <p className="md:text-xl text-l font-bold text-gray-500 mt-6">Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
+        <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
